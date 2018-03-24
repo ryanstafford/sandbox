cb867609037332739e513ca1a3202a048a45461a
Ryan Stafford <staffordrj@gmail.com>
2018-03-24 10:59:39 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-pacman -Syy tmux reflector zsh lm_sensors
+pacman -Syy reflector
 reflector --verbose --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
+pacman -Syy  zsh lm_sensors tmux
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 wget -qO- https://raw.githubusercontent.com/rystaf/sandbox/master/nocaps.map | loadkeys
