bd8d5b8cbe2f61bae521a9d603fb09a149845f93
Ryan Stafford <staffordrj@gmail.com>
2018-03-24 11:07:55 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 pacman -Syy --noconfirm reflector
 reflector --verbose --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
-pacman -Syy  --noconfirm zsh lm_sensors tmux git
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+pacman -Syy  --noconfirm zsh lm_sensors tmux
 curl https://raw.githubusercontent.com/rystaf/sandbox/master/nocaps.map | loadkeys
