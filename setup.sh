02658ef4679ea612e5d29d7d9a14460a3181af75
Ryan Stafford <staffordrj@gmail.com>
2018-03-24 10:52:20 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-pacman -Sy tmux reflector zsh
+pacman -Syy tmux reflector zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 wget -qO- https://raw.githubusercontent.com/rystaf/sandbox/master/nocaps.map | loadkeys
