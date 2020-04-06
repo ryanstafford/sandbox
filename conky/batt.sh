ce0e1e0e0e4cb682438b7dd5fe785fd005860469
Ryan Stafford <staffordrj@gmail.com>
2020-04-06 15:18:47 -0400

    battery display hide full time

--- conky/batt.sh
+++ conky/batt.sh
@@ -14,13 +14,17 @@ else
   time=$(echo "$energy/$power*60*60" | bc -l)
   icons=(          )
 fi 
-time=$(date +%T -d "1970-01-01 + $time seconds")
+if [ $(echo "$time/1" | bc) -gt 1 ]; then
+  time=$(date +%T -d "1970-01-01 + $time seconds")
+fi
 step=$(echo "x=$percentage/100*(${#icons[@]}-2); scale=0; x/1" | bc -l)
 if [ $percentage == 100 ]
 then
   step=$(echo "$step + 1" | bc)
+  echo ${icons[step]}
+else
+  echo ${icons[step]} $percentage% $time
 fi
 
-#echo ${icons[step]} $percentage% $time $life%
-echo ${icons[step]} $percentage% $time
+#echo ${icons[step]} $percentage $time $life%
 
