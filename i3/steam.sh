a885f9e046b684300e6ef3d070ef8004bbebeeb7
Ryan Stafford <staffordrj@gmail.com>
2020-09-04 11:02:55 -0400

    rofi!

--- i3/steam.sh
+++ i3/steam.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 games=$(find ~/.steam/steam/steamapps/ -maxdepth 1 -type f -name '*.acf' -exec awk -F '"' '/"appid|name/{ printf $4 "|" } END { print "" }' {} \; | column -t -s '|' | sort -k 2)
 names=$(echo "$games" | awk '{for (i=2; i<NF; i++) printf $i " "; print $NF}')
-choice=$(echo "$names" | dmenu)
+choice=$(echo "$names" | rofi -dmenu -i)
 choice_id=$(echo "$games" | grep "$choice" | awk '{print $1}')
-steam -applaunch $choice_id
+[ ! -z "$choice" ] && steam -applaunch $choice_id
